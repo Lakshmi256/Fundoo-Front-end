7fb6fc733abf753f104e633102a5430921bd0559
Lakshmi256 <lp268742@gmail.com>
2020-02-28 18:07:20 +0530

    [Lakshmiprasad]add.registration component implementing

--- Fundoo/src/app/components/register/register.component.ts
+++ Fundoo/src/app/components/register/register.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-
-
-
+import {NgForm, FormGroup,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -14,6 +11,18 @@ registerForm:FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.registerForm=new FormGroup({
+name:new FormControl(''),
+email:new FormControl(''),
+mobile:new FormControl(''),
+password:new FormControl(''),
+    })
+  }
+  onSubmit(form:NgForm){
+    if (this.registerForm.invalid){
+      return;
+    }
+    
   }
 
 }
