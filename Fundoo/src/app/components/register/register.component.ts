7a7fcf7c5bc2a6f096a333dee3e12dadf77b9d9e
Lakshmi256 <lp268742@gmail.com>
2020-02-28 11:37:23 +0530

    [Lakshmiprasad]add.register html updating

--- Fundoo/src/app/components/register/register.component.ts
+++ Fundoo/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+
 
 
 @Component({
@@ -7,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
+registerForm:FormGroup;
   constructor() { }
 
   ngOnInit(): void {
