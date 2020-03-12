467090eaec55203c256f501b91082be5b4566fae
Lakshmi256 <lp268742@gmail.com>
2020-03-12 11:58:25 +0530

    [lakshmi prasad]add.added matcard in create notes

--- Fundoo/src/environments/environment.ts
+++ Fundoo/src/environments/environment.ts
@@ -1,12 +1,18 @@
 export const environment = {
   production: false,
   apiUrl:'http://localhost:8080/users',
+  notesUrl:'http://localhost:8080/notes',
 
 
   registerUrl:'/register',
   loginUrl:'/login',
   forgotpassurl:"/forgotpassword/",
-  resetPassword:"/updatePassword"
+  resetPassword:"/updatePassword",
+  createNote:"/create",
+  updateNote:'/update',
+  pinNote:'/pin',
+  getAllNotes:'/getallnotes',
+
 
 };
 
