9733cfaf59dfe28307ab3075711798e409a9b995
Lakshmi256 <lp268742@gmail.com>
2020-03-18 10:06:26 +0530

    [lakshmi prasad]add.display and pin notes implementing

--- Fundoo/src/app/components/displaynotes/displaynotes.component.ts
+++ Fundoo/src/app/components/displaynotes/displaynotes.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { NoteServiceService } from 'src/app/service/noteservice.service';
+import { Note } from 'src/app/model/note';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-displaynotes',
@@ -7,11 +10,81 @@ import { NoteServiceService } from 'src/app/service/noteservice.service';
   styleUrls: ['./displaynotes.component.scss']
 })
 export class DisplaynotesComponent implements OnInit {
+  [x: string]: any;
+  note: Note = new Note();
+  Token = localStorage.getItem('token');
+  popup: boolean = false;
+  notes:Note[];
+  getAllNotes: [];
+  pinnotes: Note[];
+  unpinnotes:Note[];
 
-  constructor(private noteService:NoteServiceService) { }
+  private dialogref: any;
+  constructor(private noteservice: NoteServiceService, public dialog: MatDialog,public dialogRef: MatDialogRef<DisplaynotesComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.noteservice.autoRefresh$.subscribe(()=>{
+      this.displayNotes();
+    }); 
+    this.displayNotes();
+  }
+
+  public displayNotes() {
+    let getPinnedNotes = this.noteservice.getPinnedAllNote();
+    this.noteservice.getAllNote().subscribe((response: any) => {
+      console.log(response);
+      this.notes = response.data;
+    })
+    this.noteservice.getPinnedAllNote().subscribe(
+      (data) => {
+        console.log("Pin Notes"+data.data);
+        this.pinnotes = data.data;
+    })
+    this.noteservice.getAllNote().subscribe(
+      (data) => {
+        console.log("Unpin Notes"+data.data);
+        this.unpinnotes = data.data;
+      }
+    );
   }
   
+  closeClick(newNote: any) {
+    console.log(this.note.title);
+    console.log(this.note.discription);
+    this.updateNote(newNote);
+  }
+  onClickNoteId(id) {
+    this.noteservice.pinNote(id);
+  }
+  
+  openDialog(note: any) {
+    console.log("open" + note.id);
 
+  //   this.dialogref = this.dialog.open(UpdatenoteComponent, {
+  //     panelClass: 'custom-dialog-container',
+  //     width: '500px',
+  //     data: { note }
+  //   });
+  // }
+
+  // updateNote(newNote) {
+  //   this.noteservice.updateNote(newNote, localStorage.getItem('token'), this.note.id).subscribe(response => {
+  //     console.log(response.obj);
+  //     // this.dialogRef.close();
+  //   },
+  //     error => {
+  //       console.log("error");
+  //     })
+  // }
+  // token(newNote: any, id: any, token: any) {
+  //   throw new Error("Method not implemented.");
+  // }
+  // getAllPinNotes() {
+  //   this.noteservice.getAllNotes(localStorage.getItem('token')).subscribe((response: any) => {
+  //     console.log(response);
+  //     this.notes = response.obj;
+  //   });
+  // }
+  }
 }
